caa63e25cb30984d81f4944aab3ad4d96cf418f6
xorrcxrcx <134590698+Nexaleus@users.noreply.github.com>
2025-04-19 12:52:03 +0200

    Improves custom theme and build setup
    Updates the custom theme to include styling for close buttons.
    
    Also, temporarily disables theme loading due to errors.
    
    Adds custom theme files to .gitignore.
    
    Removes an unnecessary number from a comment.

--- src/main.c
+++ src/main.c
@@ -21,45 +21,45 @@ static void activate_app(GtkApplication* app, gpointer user_data) {
 // Core application logic (can be called by either main or WinMain)
 static int app_main(int argc, char **argv) {
     AdwApplication* app = NULL;
-    GtkCssProvider *provider = NULL;
-    GdkDisplay *display = NULL;
+
+    //GtkApplication* app = NULL;
+    //GtkCssProvider *provider = NULL;
+    //GdkDisplay *display = NULL;
 
     // Initialize libadwaita (important for AdwApplication)
     adw_init();
 
-    app = adw_application_new("com.example.calculator",
-                                             G_APPLICATION_DEFAULT_FLAGS);
+    app = adw_application_new("app.xorrcxrcx.calculator", G_APPLICATION_DEFAULT_FLAGS);
 
+/* Theme Loading Logic (Currently Broken)
     // Load custom CSS theme AFTER AdwApplication is created
     provider = gtk_css_provider_new();
     // Note: This assumes 'res/custom-theme.css' is accessible relative
     // to the directory where the application is run.
     // For more robust deployment, consider using GResource.
     // This version of the function doesn't report errors via GError.
-
     char* themePath = "NULL";
 
 #if defined(NDEBUG)
-themePath = "res/custom-theme.css";
+    themePath = "res/custom-theme.css";
 #else
-themePath = "../../res/custom-theme.css";
+    themePath = "../../res/custom-theme.css";
 #endif
 
+//themePath = "../../res/custom-themeETWYWJFURU";
     gtk_css_provider_load_from_path(provider, themePath);
-
     // Apply the CSS provider to the default display
     // We assume loading succeeded as this function variant doesn't report errors.
     display = gdk_display_get_default();
     gtk_style_context_add_provider_for_display(display,
                                                GTK_STYLE_PROVIDER(provider),
                                                GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-
     // The provider is referenced by the display, so we can unref it here
     g_object_unref(provider);
-
+*/
 
     g_signal_connect((GtkApplication*)app, "activate", G_CALLBACK(activate_app), NULL);
+    
     // Pass argc and argv to g_application_run
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref((GtkApplication*)app);
