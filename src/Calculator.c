b83b465c502dfda5297eba9b45f46ed44452b0bf
xorrcxrcx <134590698+Nexaleus@users.noreply.github.com>
2025-05-01 19:52:38 +0200

    Removing some commented out code section

--- src/Calculator.c
+++ src/Calculator.c
@@ -46,52 +46,6 @@ G_MODULE_EXPORT void append_to_display(GtkEntry *entry, const char *text)
 
 G_MODULE_EXPORT void calc_on_button_click(GtkWidget *button, gpointer user_data)
 {
-    /*mainApp *mApp = NULL;
-    mApp = (mainApp *)user_data;
-    if (!mApp)
-    {
-        g_error("MAINAPP POINTER IS NULL\n");
-    }
-
-    if (GTK_IS_BUTTON(button) && gtk_button_get_label(GTK_BUTTON(button)) != NULL)
-    {
-        const char *label = gtk_button_get_label(GTK_BUTTON(button));
-
-        printf("Button with label: '%s' was clicked\n", label);
-        GtkEntryBuffer *buffer = NULL;
-        buffer = gtk_entry_get_buffer(mApp->entry_handle);
-        if (!buffer)
-        {
-            g_error("ENTRY BUFFER POINTER IS NULL\n");
-        }
-
-        if (g_strcmp0(label, "C") == 0 || g_strcmp0(label, "CE") == 0)
-        {
-            if (mApp->calc->cState != CSTATE_RESET)
-            {
-                gtk_entry_buffer_set_text(buffer, "0", (int)strlen(label));
-                mApp->calc->cState = CSTATE_RESET;
-                printf("ENTRY BUFFER WAS CLEARED\n");
-            }
-            else
-            {
-                printf("ENTRY WAS ALREADY CLEARED!\n\n");
-            }
-        }
-        else
-        {
-            if(mApp->calc->cState == CSTATE_RESET)
-            {
-                mApp->calc->cState = CSTATE_INPUT_OP1;
-            }
-            gtk_entry_buffer_set_text(buffer, label, (int)strlen(label));
-        }
-    }
-    else
-    {
-        g_warning("TYPE CAST ERROR, WIDGET IS NOT OF TYPE BUTTON\n");
-    }*/
-
     mainApp *mApp = NULL;
     mApp = (mainApp *)user_data;
     if (!mApp || !mApp->calc || !mApp->entry_handle)
